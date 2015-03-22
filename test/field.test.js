339694f63f06f6961ce07b38012f7a2823d7066c
Randal Kamradt <randysr@kamradtfamily.net>
2015-03-22 13:54:17 -0700

    added user movement tests

--- test/field.test.js
+++ test/field.test.js
@@ -22,6 +22,52 @@ describe('creation and manipulation of field and user', function() {
       sut.validateUser(0,session).should.be.exactly(true);
     });
   });
+  describe('moving a user', function() {
+    it('should be move a user up and down', function() {
+      var session = 'asldfkja';
+      var sut = fieldFactory();
+      sut.makeField(session);
+      sut.addUser(session);
+      sut.moveDown(0);
+      var state = sut.getState();
+      state.should.be.instanceOf(Object).and.have.property('users');
+      state.users.should.be.instanceOf(Array);
+      state.users.should.have.length(1);
+      state.users[0].sessionid.should.be.exactly(session);
+      state.users[0].cursorx.should.be.exactly(0);
+      state.users[0].cursory.should.be.exactly(1);
+      sut.moveUp(0);
+      state = sut.getState();
+      state.should.be.instanceOf(Object).and.have.property('users');
+      state.users.should.be.instanceOf(Array);
+      state.users.should.have.length(1);
+      state.users[0].sessionid.should.be.exactly(session);
+      state.users[0].cursorx.should.be.exactly(0);
+      state.users[0].cursory.should.be.exactly(0);
+    });
+    it('should be move a user left and right', function() {
+      var session = 'asldfkja';
+      var sut = fieldFactory();
+      sut.makeField(session);
+      sut.addUser(session);
+      sut.moveRight(0);
+      var state = sut.getState();
+      state.should.be.instanceOf(Object).and.have.property('users');
+      state.users.should.be.instanceOf(Array);
+      state.users.should.have.length(1);
+      state.users[0].sessionid.should.be.exactly(session);
+      state.users[0].cursorx.should.be.exactly(1);
+      state.users[0].cursory.should.be.exactly(0);
+      sut.moveLeft(0);
+      state = sut.getState();
+      state.should.be.instanceOf(Object).and.have.property('users');
+      state.users.should.be.instanceOf(Array);
+      state.users.should.have.length(1);
+      state.users[0].sessionid.should.be.exactly(session);
+      state.users[0].cursorx.should.be.exactly(0);
+      state.users[0].cursory.should.be.exactly(0);
+    });
+  });
   describe('creation and manipulation of field', function() {
     it('should be able to make a playing field', function() {
       var sut = fieldFactory();
